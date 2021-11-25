eebb9c0efc71770b3250ea8647023ba238202846
Juniper <choirj91@gmail.com>
2021-11-25 22:30:59 +0900

    feat(beakjoon): fibonacci add

--- cnj-node-study-project/controllers/BaekjoonController.js
+++ cnj-node-study-project/controllers/BaekjoonController.js
@@ -38,9 +38,29 @@ const id10872 = async (req, res, next) => {
     });
 }
 
+// 피보나치
+const id10870 = async (req, res, next) => {
+
+    let input = require('fs').readFileSync('controllers/Baekjoon/10870.txt').toString();
+
+    console.log('input=', input);
+
+    const fibonacci = (n) => {
+        if(n <= 1) return n;
+        return fibonacci(n-2) + fibonacci(n-1);
+    }
+
+    console.log(fibonacci(input));
+
+    return res.status(200).json({
+        message: "success",
+    })
+}
+
 //********************************************************
 
 module.exports = {
     id1,
     id10872,
+    id10870,
 }
--- cnj-node-study-project/routes/baekjoon.js
+++ cnj-node-study-project/routes/baekjoon.js
@@ -6,5 +6,6 @@ const { BaekjoonController } = require('../controllers/index');
 
 router.get('', BaekjoonController.id1);
 router.get('/10872', BaekjoonController.id10872);
+router.get('/10870', BaekjoonController.id10870);
 
 module.exports = router;
