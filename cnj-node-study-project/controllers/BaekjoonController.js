6eca353163d56f095e1b9232d6fbe338f6ed4a83
Juniper <choirj91@gmail.com>
2021-12-01 22:22:33 +0900

    feat(beakjoon): recursive function 2447 add

--- cnj-node-study-project/controllers/BaekjoonController.js
+++ cnj-node-study-project/controllers/BaekjoonController.js
@@ -18,8 +18,8 @@ const id10872 = async (req, res, next) => {
 
     // 일반 재귀
     const factorial = (input) => {
-        if(input <= 1) return input;
-        return input * fac(input -1);
+        if (input <= 1) return input;
+        return input * fac(input - 1);
     }
 
     // 꼬리 재귀
@@ -46,11 +46,46 @@ const id10870 = async (req, res, next) => {
     console.log('input=', input);
 
     const fibonacci = (n) => {
-        if(n <= 1) return n;
-        return fibonacci(n-2) + fibonacci(n-1);
+        if (n <= 1) return n;
+        return fibonacci(n - 2) + fibonacci(n - 1);
     }
 
     console.log(fibonacci(input));
+
+    return res.status(200).json({
+        message: "success",
+    })
+}
+
+// 별찍기 재귀 함수
+const id2447 = async (req, res, next) => {
+
+    let input = require('fs').readFileSync('controllers/Baekjoon/2447.txt').toString();
+
+    const num = parseInt(input);
+    const lines = []
+
+    console.log('input=', input);
+
+    const printStars = (num) => {
+        for (let i = 0; i < num; i++) {
+            for (let j = 0; j < num; j++) {
+                star(i, j, num)
+            }
+            lines.push("\n")
+        }
+    }
+
+    const star = (i, j, num) => {
+        if (i % 3 == 1 && j % 3 == 1) lines.push(" ");
+        else {
+            if (num == 1) lines.push("*");
+            else star(parseInt(i / 3), parseInt(j / 3), parseInt(num / 3));
+        }
+    }
+
+    printStars(num);
+    console.log(lines.join(""))
 
     return res.status(200).json({
         message: "success",
@@ -63,4 +98,5 @@ module.exports = {
     id1,
     id10872,
     id10870,
+    id2447,
 }
--- cnj-node-study-project/routes/baekjoon.js
+++ cnj-node-study-project/routes/baekjoon.js
@@ -7,5 +7,6 @@ const { BaekjoonController } = require('../controllers/index');
 router.get('', BaekjoonController.id1);
 router.get('/10872', BaekjoonController.id10872);
 router.get('/10870', BaekjoonController.id10870);
+router.get('/2447', BaekjoonController.id2447);
 
 module.exports = router;
